a6a9cd21aa89666ea7cb0bbe0c3af8f18db3f4b6
adrianh <adrianh@d34b534f-cdcc-0310-9fd0-f1c7c63e638c>
2005-06-04 23:56:49 +0000

    changes for 0.21

--- lib/Test/Exception.pm
+++ lib/Test/Exception.pm
@@ -53,6 +53,17 @@ Test::Exception - Test exception based code
 
   # Check that a test runs without an exception
   lives_and {is $foo->method, 42} 'method is 42';
+  
+  # or if you don't like prototyped functions
+  
+  dies_ok( sub { $foo->method1 }, 'expecting to die' );
+  lives_ok( sub {$foo->method2}, 'expecting to live' );
+  throws_ok( sub {$foo->method3}, qr/division by zero/,
+      'zero caught okay' );
+  throws_ok( sub {$foo->method4}, 'Error::Simple', 
+      'simple error thrown' );
+  lives_and( sub {is $foo->method, 42}, 'method is 42' );
+
 
 =head1 DESCRIPTION
 
@@ -102,18 +113,20 @@ Checks that a piece of code dies, rather than returning normally. For example:
     };
 
     dies_ok { div(1, 0) } 'divide by zero detected';
+    # or if you don't like prototypes
+    dies_ok( sub { div(1, 0) }, 'divide by zero detected' );
 
 A true value is returned if the test succeeds, false otherwise. On exit $@ is guaranteed to be the cause of death (if any).
 
-The test name is optional, but recommended. 
+The test description is optional, but recommended. 
 
 =cut
 
 
 sub dies_ok (&;$) {
-    my ($coderef, $name) = @_;
+    my ($coderef, $description) = @_;
     my $exception = _try_as_caller($coderef);
-    my $ok = $Tester->ok( _is_exception($exception), $name );
+    my $ok = $Tester->ok( _is_exception($exception), $description );
     $@ = $exception;
     return($ok);
 }
@@ -134,6 +147,8 @@ Checks that a piece of code exits normally, and doesn't die. For example:
 
     my $file;
     lives_ok { $file = read_file('test.txt') } 'file read';
+    # or if you don't like prototypes
+    lives_ok( sub { $file = read_file('test.txt') }, 'file read' );
 
 Should a lives_ok() test fail it produces appropriate diagnostic messages. For example:
 
@@ -143,14 +158,14 @@ Should a lives_ok() test fail it produces appropriate diagnostic messages. For e
 
 A true value is returned if the test succeeds, false otherwise. On exit $@ is guaranteed to be the cause of death (if any).
 
-The test name is optional, but recommended. 
+The test description is optional, but recommended. 
 
 =cut
 
 sub lives_ok (&;$) {
-    my ($coderef, $name) = @_;
+    my ($coderef, $description) = @_;
     my $exception = _try_as_caller($coderef);
-    my $ok = $Tester->ok(! _is_exception($exception), $name)
+    my $ok = $Tester->ok(! _is_exception($exception), $description)
         || $Tester->diag(_exception_as_string("died:", $exception));
     $@ = $exception;
     return($ok);
@@ -161,19 +176,19 @@ sub lives_ok (&;$) {
 
 Tests to see that a specific exception is thrown. throws_ok() has two forms: 
 
-  throws_ok BLOCK REGEX, TEST_NAME
-  throws_ok BLOCK CLASS, TEST_NAME
+  throws_ok BLOCK REGEX, TEST_DESCRIPTION
+  throws_ok BLOCK CLASS, TEST_DESCRIPTION
 
 In the first form the test passes if the stringified exception matches the give regular expression. For example:
 
     throws_ok { 
-        read_file('test.txt') 
+        read_file('unreadable') 
     } qr/No such file/, 'no file';
 
 If your perl does not support C<qr//> you can also pass a regex-like string, for example:
 
     throws_ok { 
-        read_file('/etc/kcpassword') 
+        read_file('unreadable') 
     } '/Permission denied/', 'no permissions';
 
 The second form of throws_ok() test passes if the exception is of the same class as the one supplied, or a subclass of that class. For example:
@@ -194,21 +209,25 @@ Should a throws_ok() test fail it produces appropriate diagnostic messages. For 
     # expecting: Error::Simple exception
     # found: normal exit
 
+Like all other Test::Exception functions you can avoid protypes by passing a subroutine explicitly:
+
+    throws_ok( sub {$foo->bar}, "Error::Simple", 'simple error' );
+
 A true value is returned if the test succeeds, false otherwise. On exit $@ is guaranteed to be the cause of death (if any).
 
-The test name is optional. If no test name is given a description of the exception being checked for is used. 
+A description of the exception being checked is used if no optional test description is passed.
 
 =cut
 
 
 sub throws_ok (&$;$) {
-    my ($coderef, $expecting, $name) = @_;
-    $name ||= _exception_as_string("threw", $expecting);
+    my ($coderef, $expecting, $description) = @_;
+    $description ||= _exception_as_string("threw", $expecting);
     my $exception = _try_as_caller($coderef);
     my $regex = $Tester->maybe_regex($expecting);
     my $ok = $regex ? ($exception =~ m/$regex/) 
             : UNIVERSAL::isa($exception, ref($expecting) || $expecting);
-    $Tester->ok($ok, $name);
+    $Tester->ok($ok, $description);
     unless ($ok) {
         $Tester->diag( _exception_as_string("expecting:", $expecting) );
         $Tester->diag( _exception_as_string("found:", $exception) );
@@ -222,38 +241,40 @@ sub throws_ok (&$;$) {
 
 Run a test that may throw an exception. For example, instead of doing:
 
-    my $file;
-    lives_ok { $file = read_file('answer.txt') } 'read_file worked';
-    is $file, "42\n", 'answer was 42';
+  my $file;
+  lives_ok { $file = read_file('answer.txt') } 'read_file worked';
+  is $file, "42", 'answer was 42';
 
 You can use lives_and() like this:
 
-    lives_and { is read_file('answer.txt'), "42\n" } 'answer is 42';
+  lives_and { is read_file('answer.txt'), "42" } 'answer is 42';
+  # or if you don't like prototypes
+  lives_and(sub {is read_file('answer.txt'), "42"}, 'answer is 42');
 
 Which is the same as doing
 
-    is read_file('answer.txt'), "42\n", 'answer is 42';
+  is read_file('answer.txt'), "42\n", 'answer is 42';
 
 unless C<read_file('answer.txt')> dies, in which case you get the same kind of error as lives_ok()
 
-    not ok 1 - answer is 42
-    #     Failed test (test.t at line 15)
-    # died: open failed (No such file or directory)
+  not ok 1 - answer is 42
+  #     Failed test (test.t at line 15)
+  # died: open failed (No such file or directory)
 
 A true value is returned if the test succeeds, false otherwise. On exit $@ is guaranteed to be the cause of death (if any).
 
-The test name is optional, but recommended.
+The test description is optional, but recommended.
 
 =cut
 
 sub lives_and (&$) {
-    my ($test, $name) = @_;
+    my ($test, $description) = @_;
     {
         local $Test::Builder::Level = $Test::Builder::Level+1;
         my $ok = \&Test::Builder::ok;
         no warnings;
         local *Test::Builder::ok = sub {
-            $_[2] = $name unless defined $_[2];
+            $_[2] = $description unless defined $_[2];
             $ok->(@_);
         };
         use warnings;
@@ -261,7 +282,7 @@ sub lives_and (&$) {
     };
     my $exception = $@;
     if (_is_exception($exception)) {
-        $Tester->ok(0, $name);
+        $Tester->ok(0, $description);
         $Tester->diag( _exception_as_string("died:", $exception) );
     };
     $@ = $exception;
@@ -278,18 +299,33 @@ None known at the time of writing.
 If you find any please let me know by e-mail, or report the problem with L<http://rt.cpan.org/>.
 
 
+=head1 COMMUNITY
+
+=head2 perl-qa
+
+If you are interested in testing using Perl I recommend you visit L<http://qa.perl.org/> and join the excellent perl-qa mailing list. See L<http://lists.perl.org/showlist.cgi?name=perl-qa> for details on how to subscribe.
+
+=head2 perlmonks
+
+You can find users of Test::Exception, including the module author, on  L<http://www.perlmonks.org/>. Feel free to ask questions on Test::Exception there.
+
+=head2 CPAN::Forum
+
+The CPAN Forum is a web forum for discussing Perl's CPAN modules.   The Test::Exception forum can be found at L<http://www.cpanforum.com/dist/Test-Exception>.
+
+
 =head1 TO DO
 
-Nothing at the time of writing.
+If you think this module should do something that it doesn't (or does something that it shouldn't) please let me know.
 
-If you think this module should do something that it doesn't do at the moment please let me know.
+You can see my current to do list at L<http://adrianh.tadalist.com/lists/public/15421>, with an RSS feed of changes at L<http://adrianh.tadalist.com/lists/feed_public/15421>.
 
 
 =head1 ACKNOWLEGEMENTS
 
 Thanks to chromatic and Michael G Schwern for the excellent Test::Builder, without which this module wouldn't be possible.
 
-Thanks to Michael G Schwern, Mark Fowler, Janek Schleicher, chromatic, Mark Fowler, Peter Scott, Aristotle and Andy Lester for suggestions, bug reports and patches.
+Thanks to Michael G Schwern, Mark Fowler, Janek Schleicher, chromatic, Peter Scott, Aristotle, Andy Lester, David Wheeler, Jos I. Boumans, Jim Keenan & Perrin for comments, suggestions, bug reports and patches.
 
 
 =head1 AUTHOR
