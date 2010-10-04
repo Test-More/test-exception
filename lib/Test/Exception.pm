145bbd21342dfacbdb5c91a6ea27b004b885a054
Adrian Howard <adrianh@quietstars.com>
2010-10-04 12:29:21 +0100

    -   Upped version number -   Added Peter Rabbitson to the acknowledgements section -   Added some clarifying documentation to respond to RT#59293

--- lib/Test/Exception.pm
+++ lib/Test/Exception.pm
@@ -198,6 +198,11 @@ A true value is returned if the test succeeds, false otherwise. On exit $@ is gu
 
 A description of the exception being checked is used if no optional test description is passed.
 
+NOTE: Rememeber when you C<die $string_without_a_trailing_newline> perl will 
+automatically add the current script line number, input line number and a newline. This will
+form part of the string that throws_ok regular expressions match against.
+
+
 =cut
 
 
@@ -438,6 +443,7 @@ Michael G Schwern,
 Nadim Khemir,
 Paul McCann,
 Perrin Harkins, 
+Peter Rabbitson,
 Peter Scott, 
 Ricardo Signes,
 Rob Muhlestein 
